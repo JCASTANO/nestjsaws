5f973fb7e4284d07fb5ee82a49daa9626a36aac0
Juan Esteban <juan.castano@ceiba.com.co>
2020-04-15 15:07:50 -0500

    GRAL:Ajuste

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { InfraestructuraModule } from './infraestructura/infraestructura.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [InfraestructuraModule],
+  controllers: [AppController]
 })
 export class AppModule {}
